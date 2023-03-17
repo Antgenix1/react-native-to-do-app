cb9181a9b73b8dc1216df779920e898fc6e286b2
aro135603 <aro135603@gibb.ch>
2023-03-17 11:19:45 +0100

    chore: work

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button } from 'react-native';
+import { TextInput, Button, FlatList } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
+const DATA = [
+  {
+    id: 1,
+    title: 'First Item',
+  },
+  {
+    id: 2,
+    title: 'Second Item',
+  },
+  {
+    id: 3,
+    title: 'Third Item',
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
 export default function App() {
+  const [text, onChangeText] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>TO-DO</Text>
+      <Text>Add a Todo to your List</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Todo"/>
+        <Button
+        title='Add'/>
+        <FlatList
+          data={DATA}
+          renderItem={({item}) => <Item title={item.title}/>}
+            keyExtractor={item => item.id}
+          />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +51,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D7D9D8',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  item: {
+    backgroundColor: '#D9B341',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
